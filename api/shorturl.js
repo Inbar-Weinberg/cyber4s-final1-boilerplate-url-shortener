d9fa58b984eaed4174ad865f85cbb24256815d07
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-03-04 16:21:10 +0200

    finished Error responses for PUT
    PUT sends back status responses 200,400,500 and displays them back to user.

--- api/shorturl.js
+++ api/shorturl.js
@@ -1,32 +1,43 @@
+//-- packages used
 const express = require("express");
 const fsPromises = require("fs/promises");
 const dataTemplate = require(`${process.cwd()}/templates/urlDataTemplate`);
+const validator = require("validator");
+
 const router = express.Router();
 const URL_DATABASE_dir = `${process.cwd()}/database/url-data.json`;
-
+//--
 router.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+//--get,post
 router.get("/", async (req, res) => {});
-//
 
 router.post("/new", async (req, res) => {
   const newUrlData = new dataTemplate(req.body.url);
-  try {
-    const allUrls = JSON.parse(await fsPromises.readFile(URL_DATABASE_dir));
-    allUrls.urls.push(newUrlData);
-    await fsPromises.writeFile(
-      URL_DATABASE_dir,
-      JSON.stringify(allUrls, null, 4)
-    );
-    res.status(200);
-    res.redirect("back");
-    //add error notes
-  } catch (error) {
-    console.log(error);
+  if (validator.isURL(req.body.url)) {
+    try {
+      const allUrls = JSON.parse(await fsPromises.readFile(URL_DATABASE_dir));
+      allUrls.urls.push(newUrlData);
+      await fsPromises.writeFile(
+        URL_DATABASE_dir,
+        JSON.stringify(allUrls, null, 4)
+      );
+      res.status(200);
+      res.redirect(`../../${res.statusCode}/${newUrlData.shortUrl}`);
+    } catch (err) {
+      res.status(500);
+      res.redirect(`../../${res.statusCode}/${err}`);
+    }
+  } else {
+    res.status(400);
+    const message = "The URL sent is not valid.";
+    res.redirect(`../../${res.statusCode}/${message}`);
   }
 });
 
+//--exports
 module.exports = router;
--- app.js
+++ app.js
@@ -6,35 +6,29 @@ const api = require("./api");
 const fsPromises = require("fs/promises");
 const URL_DATABASE_dir = `${process.cwd()}/database/url-data.json`;
 
-
-
 //----
 app.set("view engine", "ejs");
 
 //---
 app.use(cors()); // allow access to write files
-app.use("/public", express.static(`./public`)); 
+app.use("/public", express.static(`./public`));
 
 //--
-app.get("/", async (req, res) => {
-  try {
-    const allUrls = JSON.parse(await fsPromises.readFile(URL_DATABASE_dir))
-      .urls;
-      console.log('-----')
-
-      console.log(allUrls)
-    res.render("index", { allUrls: allUrls });
-  } catch (error) {
-    console.log(error);
+app.get("/(:id)?(/:url)?", async (req, res) => {
+  const status = req.params.id;
+  if (!isNaN(status) || !status) {
+    const url = req.params.url;
+    try {
+      const allUrls = JSON.parse(await fsPromises.readFile(URL_DATABASE_dir))
+        .urls;
+      res.render("index", { allUrls, status, url });
+    } catch (error) {
+      console.log(error);
+    }
   }
+ // res.redirect(`/${req.params.id}`)
 });
 
-
-app.use('/api',api);
-
-
+app.use("/api", api);
 
 module.exports = app;
-
-
-
